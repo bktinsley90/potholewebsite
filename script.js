289c6a40259f00241f05e9cf6493b7610dc87229
Brittany Tinsley <brittanytinsley@Brittanys-MBP.jetpack>
2020-03-30 17:10:19 -0400

    lost on javacript

---
+++ script.js
@@ -0,0 +1,13 @@
+//  need to write a function that checks credntial 
+function logIn(){
+    let userName = prompt(`Enter Username`);
+    let password = prompt(`Enterpassword`);
+    function logMein(userName, password){
+        if (userName === "admin"){
+            
+        }
+    }
+
+
+}
+//  need to write another function that will input the name of a pothole and print on the page
